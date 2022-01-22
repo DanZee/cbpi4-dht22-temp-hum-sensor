381cf15163638a179cb47350be328ea2517d0b97
¨Daniel¨ <daniel@ezee.org>
2022-01-22 16:35:15 +0100

    Relabel logger

--- cbpi4-dht22-temp-hum-sensor/__init__.py
+++ cbpi4-dht22-temp-hum-sensor/__init__.py
@@ -8,7 +8,8 @@ import time
 import board
 from microcontroller import Pin
 
-logger = logging.getLogger(__name__)
+
+logger = logging.getLogger("cbpi4-dht22")
 
 @parameters([Property.Select(label="cpioPin", options=[0, 1, 2, 3, 4, 5, 6, 7, 8, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27], description="GPIO Pin connected to DHT22 data port"),
     Property.Select(label="Type", options = ["Humidity","Temperature"],description = "Select sensor type"),
@@ -35,7 +36,6 @@ class CpbiDht22(CBPiSensor):
         self.dhtDevice = adafruit_dht.DHT22(Pin(self.cpioPin), use_pulseio=False)
         while self.running == True:
             try:
-
                 if self.Type == "Temperature":
                     self.value = self.dhtDevice.temperature
                 else:
